75dec381685d96354919bcc43995ab986a27e287
avinanick <avinanick@gmail.com>
2020-01-20 12:30:51 -0800

    Created the initial test for reading the word grid and list of words into the word search class.

---
+++ WordSearchTest.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class WordSearchTestCase(unittest.TestCase):
+    def test_reading_search_grid_from_file_to_array(self):
+        word_search = WordSearch()
+        word_search.read_file("TestWordGrid.csv")
+        self.assertEqual("Z", word_search.grid[5][1], "Test grid not correct!")
+        self.assertIn("KIRK", word_search.target_words, "Did not successfully read target words!")
+        self.assertEqual(7, word_search.target_words.size(), "Did not successfully read all target words!")
+
+
+if __name__ == '__main__':
+    unittest.main()

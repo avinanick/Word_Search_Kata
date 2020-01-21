a57cc78dfe4e520c12f9cda51760bd2b1a1d12e5
avinanick <avinanick@gmail.com>
2020-01-20 16:20:54 -0800

    Created the test for finding horizontal words in the word search grid.

--- WordSearchTest.py
+++ WordSearchTest.py
@@ -3,12 +3,21 @@ from WordSearch import WordSearch
 
 
 class WordSearchTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.word_search = WordSearch()
+
     def test_reading_search_grid_from_file_to_array(self):
-        word_search = WordSearch()
-        word_search.read_file("TestWordGrid.csv")
-        self.assertEqual("Z", word_search.grid[5][1], "Test grid not correct!")
-        self.assertIn("KIRK", word_search.target_words, "Did not successfully read target words!")
-        self.assertEqual(7, len(word_search.target_words), "Did not successfully read all target words!")
+        self.word_search.read_file("TestWordGrid.csv")
+        self.assertEqual("Z", self.word_search.grid[5][1], "Test grid not correct!")
+        self.assertIn("KIRK", self.word_search.target_words, "Did not successfully read target words!")
+        self.assertEqual(7, len(self.word_search.target_words), "Did not successfully read all target words!")
+
+    def test_word_search_finds_horizontal_words(self):
+        self.word_search.read_file("TestHorizontalWordGrid.csv")
+        self.assertIn("LINK", self.word_search.solved_words)
+        self.assertIn((2,1), self.word_search.solved_words["LINK"])
+        self.assertIn("FIRE", self.word_search.solved_words)
+        self.assertIn((1, 4), self.word_search.solved_words["FIRE"])
 
 
 if __name__ == '__main__':

c130e6bf5785c4f78fc13305fc66054ea3645030
= <avinanick@gmail.com>
2020-01-20 19:13:55 -0800

    First attempt at the word search solving functions, but it's causing an infinite loop somewhere in there.

--- WordSearch.py
+++ WordSearch.py
@@ -26,4 +26,35 @@ class WordSearch():
                         self.grid[j].append(letter)
 
     def solve_grid(self):
-        return #STUB
+        for current_word in self.target_words:
+            word_coords = []
+            # Search the grid until we find the first letter of the word
+            for x, row in enumerate(self.grid):
+                for y, letter in enumerate(row):
+                    if letter == current_word[0]:
+                        # Once we find the first letter, begin a depth-first search in all directions for the rest of the word
+                        word_coords.append((x,y))
+                        x_direction = 0
+                        letter_index_in_word = 1
+                        search_coords_stack = []
+                        search_coords_stack.append((x-1, y))
+                        search_coords_stack.append((x+1, y))
+                        while len(word_coords) < len(current_word) and len(search_coords_stack) > 0:
+                            next_coord = search_coords_stack.pop()
+                            if x < len(self.grid) and y < len(self.grid[x]):
+                                current_x_direction = next_coord[0] - x
+                                if current_x_direction != 0:
+                                    current_x_direction = current_x_direction / abs(current_x_direction)
+                                if current_x_direction != x_direction:
+                                    letter_index_in_word = 1
+                                    word_coords = [(x,y)]
+                                else:
+                                    letter_index_in_word += 1
+                                if self.grid[next_coord[0]][next_coord[y]] == current_word[letter_index_in_word]:
+                                    word_coords.append(next_coord)
+                                    if len(word_coords) < len(current_word):
+                                        search_coords_stack.append((next_coord[0] + x_direction, next_coord[1]))
+                                    else:
+                                        # Found the whole word!
+                                        self.solved_words[current_word] = word_coords
+                                        print(current_word, " : ", word_coords)
